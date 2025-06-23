1beec68b58096ba0dfd130cd79be6086398613d2
mkaif8417 <mkaif6937@gmail.com>
2025-06-23 19:50:42 +0530

    Some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,71 @@
 import { useState } from "react"
 
-
 function App() {
-  const [color,setcolor]=useState("olive")
+  const [color, setColor] = useState("olive")
 
   return (
-   <div className="w-full h-screen duration-200"
-style={{backgroundColor:color}}
-   >
-    {/* <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2"> */}
-   
-    <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-xl">
- 
-<button
-onClick={()=>setcolor("red")}
-className="hover:text-sm outline-none px-4 rounded-full text-white shadow-lg"
-style={{backgroundColor:"red"}}>Red</button>
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-xl fixed bottom-12 inset-x-0">
+        <button
+          onClick={() => setColor("red")}
+          className="outline-none px-4 rounded-full text-white shadow-lg cursor-pointer"
+          style={{ backgroundColor: "red" }}
+        >
+          Red
+        </button>
 
-<button 
-onClick={()=>setcolor("green")}
-className=" hover:text-smoutline-none px-4 rounded-full text-white shadow-lg "style={{backgroundColor:"green"}}>Green</button>
+        <button
+          onClick={() => setColor("green")}
+          className="outline-none px-4 rounded-full text-white shadow-lg cursor-pointer"
+          style={{ backgroundColor: "green" }}
+        >
+          Green
+        </button>
 
-<button
-onClick={()=>setcolor("yellow")}
- className="hover:text-sm outline-none px-4 rounded-full text-white shadow-lg "style={{backgroundColor:"yellow"}}>Yellow</button>
+        <button
+          onClick={() => setColor("yellow")}
+          className="outline-none px-4 rounded-full shadow-lg cursor-pointer"
+          style={{ backgroundColor: "yellow", color: "black" }}
+        >
+          Yellow
+        </button>
 
-<button
-onClick={()=>setcolor("pink")}
-className="hover:text-sm outline-none px-4 rounded-full text-white shadow-lg "style={{backgroundColor:"pink"}}>Pink</button>
+        <button
+          onClick={() => setColor("pink")}
+          className="outline-none px-4 rounded-full text-white shadow-lg cursor-pointer"
+          style={{ backgroundColor: "pink" }}
+        >
+          Pink
+        </button>
 
-<button 
-onClick={()=>setcolor("purple")}
-className="hover:text-sm outline-none px-4 rounded-full text-white shadow-lg "style={{backgroundColor:"purple"}}>Purple</button>
+        <button
+          onClick={() => setColor("purple")}
+          className="outline-none px-4 rounded-full text-white shadow-lg cursor-pointer"
+          style={{ backgroundColor: "purple" }}
+        >
+          Purple
+        </button>
 
+        <button
+          onClick={() => setColor("black")}
+          className="outline-none px-4 rounded-full text-white shadow-lg cursor-pointer"
+          style={{ backgroundColor: "black" }}
+        >
+          Black
+        </button>
 
-<button 
-onClick={()=>setcolor("black")}
-className="hover:text-sm outline-none px-4 rounded-full text-white shadow-lg "style={{backgroundColor:"black"}}>Black</button>
-
-
-<button 
-onClick={()=>setcolor("grey")}
-className="hover:text-sm outline-none px-4 rounded-full text-white shadow-lg "style={{backgroundColor:"grey"}}>Grey</button>
-    {/* </div> */}
-   </div>
-   </div>
+        <button
+          onClick={() => setColor("grey")}
+          className="outline-none px-4 rounded-full text-white shadow-lg cursor-pointer"
+          style={{ backgroundColor: "grey" }}
+        >
+          Grey
+        </button>
+      </div>
+    </div>
   )
 }
 
