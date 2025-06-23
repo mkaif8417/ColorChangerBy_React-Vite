b73acf29b8bab46f53e79557c58643ca38cebf8c
mkaif8417 <mkaif6937@gmail.com>
2025-06-23 19:38:07 +0530

    Fixing some errors

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/04colorchanger/',  // replace with your repo name if different
+  plugins: [react()]
 })
